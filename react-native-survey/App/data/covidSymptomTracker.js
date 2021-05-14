ff7979e9b27e585db9b6669979298b8c460492e2
sgskhare <sgskhare@liverpool.ac.uk>
2021-05-14 12:56:49 +0100

    Initial Commit

---
+++ react-native-survey/App/data/covidSymptomTracker.js
@@ -0,0 +1,79 @@
+const questions = [
+    {
+        question: "Have you had Covid19 for more than 28 days?",
+        answers: [
+            { id: "1", text: "Yes", correct: true },
+            { id: "2", text: "No", correct: false },
+        ]
+    },
+    {
+        question: "Have you had Covid19 for more than 56 days?",
+        answers: [
+            { id: "1", text: "Yes", correct: true },
+            { id: "2", text: "No", correct: false },
+        ]
+    },
+    {
+        question: "Did you have cough when you were infected with covid?",
+        answers: [
+            { id: "1", text: "Yes", correct: true },
+            { id: "2", text: "No", correct: false },
+        ]
+    },
+    {
+        question: "Did you have Fever when you were infected with covid?",
+        answers: [
+            { id: "1", text: "Yes", correct: true },
+            { id: "2", text: "No", correct: false },
+        ]
+    },
+    {
+        question: "Did you have Sore throat when you were infected with covid?",
+        answers: [
+            { id: "1", text: "Yes", correct: true },
+            { id: "2", text: "No", correct: false },
+        ]
+    },
+    {
+        question: "Did you have Shortness of breadth when you were infected with covid?",
+        answers: [
+            { id: "1", text: "Yes", correct: true },
+            { id: "2", text: "No", correct: false },
+        ]
+    },
+    {
+        question: "Did you have Head ache when you were infected with covid?",
+        answers: [
+            { id: "1", text: "Yes", correct: true },
+            { id: "2", text: "No", correct: false },
+        ]
+    },
+    {
+        question: "What is your age please choose",
+        answers: [
+            { id: "1", text: "20-30", correct: false },
+            { id: "2", text: "31-40", correct: false },
+            { id: "3", text: "41-50", correct: false },
+            { id: "4", text: "51-60", correct: false },
+            { id: "5", text: "Above 60", correct: true },
+        ]
+    },
+    {
+        question: "What is you Gender please choose",
+        answers: [
+            { id: "1", text: "Male", correct: true },
+            { id: "2", text: "Female", correct: false },
+            { id: "3", text: "Other", correct: false },
+            { id: "4", text: "Prefer not to say", correct: false },
+        ]
+    },
+    {
+        question: "Did you have Asthama",
+        answers: [
+            { id: "1", text: "Yes", correct: true },
+            { id: "2", text: "No", correct: false },
+        ]
+    }
+];
+
+export default questions;
